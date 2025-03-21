a29e98e1fbfe707d0aa4b7e9cb17e75f49112bcd
Marisi Romanillos <109613071+marisiromanillos@users.noreply.github.com>
2025-03-21 10:30:56 +0000

    graphql running, queries working, assing types to my mock location data

--- index.js
+++ index.js
@@ -1,53 +1,73 @@
 const express = require("express");
-const graphql = require("graphql");
 const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
-  GraphQLID,
-  GraphQLInputObjectType,
+  GraphQLFloat,
+  GraphQLList,
   GraphQLInt,
 } = require("graphql");
-const { graphqlHTTP } = require(express - graphql);
+const { graphqlHTTP } = require("express-graphql");
 const locationData = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 4000;
 
 const LocationType = new GraphQLObjectType({
   name: "Location",
-  fields: () => {
-    id: {
-      type: GraphQLInt;
-    }
-    name: {
-      type: GraphQLString;
-    }
-    address: {
-      type: GraphQLString;
-    }
-    coordinates: {
-      type: GraphQLInputObjectType;
-    }
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    address: { type: GraphQLString },
+    coordinates: { type: new GraphQLList(GraphQLFloat) },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllLocations: {
+      type: new GraphQLList(LocationType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return locationData;
+      },
+    },
   },
 });
 
-// root query
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {},
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createLocation: {
+      type: LocationType,
+      args: {
+        name: { type: GraphQLString },
+        address: { type: GraphQLString },
+        coordinates: { type: new GraphQLList(GraphQLFloat) },
+      },
+      resolve(parent, args) {
+        locationData.push({
+          id: locationData.length + 1,
+          name: args.name,
+          address: args.address,
+          coordinates: args.coordinates,
+        });
+        return args;
+      },
+    },
+  },
 });
-const Mutation = "mutation";
 
-// create graphql server
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
 app.use(
-  "./graphql",
+  "/graphql",
   graphqlHTTP({
     schema,
     graphiql: true,
   })
 );
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
